13db5fc4e7cc936b8bcefb0dae34aac1a96c22b3
Drozed <david.osorio5630@gmail.com>
2021-08-31 18:24:27 -0500

    Primer Commit

---
+++ SalesSystem/Data/Migrations/20210831024413_MIGRACIONLIVRE.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesSystem.Data.Migrations
+{
+    public partial class MIGRACIONLIVRE : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
